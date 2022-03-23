16c3ec5945742764a397a64016c5b019a525f48b
Victor Martinez <victordmtz@hotmail.com>
2022-03-23 11:40:43 -0700

    Adds basic files

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>ENLACE LLC</title>
+    <meta charset="utf-8">
+  </head>
+    <?php echo "this is a php tag"?>
+  <body>
+
+  </body>
+</html>
